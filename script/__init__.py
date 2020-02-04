d6c7e3193be714c0ccbf6feaa4dae29800cdc218
Ilya Smolin <54890790+smolinilya01@users.noreply.github.com>
2020-02-04 10:35:33 +0600

    Buiding skeletons

---
+++ script/__init__.py
@@ -0,0 +1,1 @@
+"""Calculation metal script"""
---
+++ script/run.py
@@ -0,0 +1,10 @@
+"""Run calculation metal script"""
+
+
+def main() -> None:
+    """Run script"""
+    pass
+
+
+if __name__ == "__main__":
+    main()
